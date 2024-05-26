a8a8cca74c666f96daf8778249bf7b53887ccba6
Aia Dagkesamanskaia <a.dagkes@ya.ru>
2024-05-26 20:39:06 +0400

    Color pallet to add and Hex2NameColor to choose

--- cats/models.py
+++ cats/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+CHOICES = (
+        ('Gray', 'Серый'),
+        ('Black', 'Чёрный'),
+        ('White', 'Белый'),
+        ('Ginger', 'Рыжий'),
+        ('Mixed', 'Смешанный'),
+    )
+
+
 class Achievement(models.Model):
     name = models.CharField(max_length=64)
 
